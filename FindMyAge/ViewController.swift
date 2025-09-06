242dcb27123b99e11136382bdedbcae976139f76
Abhay Dixit <dixitabhay1104@gmail.com>
2025-09-06 22:32:00 +0530

    An ios app for finding age of a person

--- FindMyAge/ViewController.swift
+++ FindMyAge/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate{
+    
+    @IBOutlet weak var lbAge: UILabel!
+    @IBOutlet weak var txtyearofbirth: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
+    
+    @IBAction func btFindAge(_ sender: Any) {
+            getAge()
+        }
+    func getAge(){
+        if let birthYearText = txtyearofbirth.text,
+                          let yearofBirth = Int(birthYearText) {
+                        let date = Date()
+                        let calendar = Calendar.current
+                        let year = calendar.component(.year, from: date)
+                           let personAge = year - yearofBirth
+                           lbAge.text = "Your age is \(personAge) years"
+                       } else {
+                           lbAge.text = "Please enter a valid year"
+                       }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        getAge()
+        self.view.endEditing(true)
+        return true
+    }
+    }
 
